0f619b2c696177f5718a5acfb06ccc9df082eed4
scrawach <scrawach@gmail.com>
2025-09-09 17:52:43 +0300

    feat: make some smooth animation for aiming

--- src/interactions/clicked_on_area.gd
+++ src/interactions/clicked_on_area.gd
@@ -9,5 +9,5 @@ func _ready() -> void:
 	area.input_event.connect(_on_input_event)
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	if event.is_action_pressed("debug_interact"):
+	if event.is_action_pressed("interact"):
 		clicked.emit(area)
