6745330fc599a10f93ee426d4f324c487ede8985
scrawach <scrawach@gmail.com>
2025-09-10 16:21:05 +0300

    refactor: use clickable area3d instead of clicked on area node in table lable

--- src/table/table_label.gd
+++ src/table/table_label.gd
@@ -14,8 +14,7 @@ signal clicked()
 @export var hover_duration: float = 0.2
 
 @onready var label_3d: Label3D = %Label3D
-@onready var area_3d: Area3D = %Area3D
-@onready var clicked_on_area3d = %ClickedOnArea3D
+@onready var clickable_area_3d: ClickableArea3D = %"Clickable Area3d"
 
 var tween: Tween
 
@@ -31,13 +30,13 @@ func _on_mouse_exited() -> void:
 	tween.tween_property(label_3d, "modulate", normal_color, hover_duration)
 	tween.parallel().tween_property(label_3d, "scale", Vector3.ONE, hover_duration)
 
-func _on_clicked(_area: Area3D) -> void:
+func _on_clicked() -> void:
 	clicked.emit()
 
 func smooth_show() -> void:
-	area_3d.mouse_entered.connect(_on_mouse_entered)
-	area_3d.mouse_exited.connect(_on_mouse_exited)
-	clicked_on_area3d.clicked.connect(_on_clicked)
+	clickable_area_3d.mouse_entered.connect(_on_mouse_entered)
+	clickable_area_3d.mouse_exited.connect(_on_mouse_exited)
+	clickable_area_3d.clicked.connect(_on_clicked)
 	
 	label_3d.modulate.a = 0
 	show()
@@ -46,9 +45,9 @@ func smooth_show() -> void:
 	tween.tween_property(label_3d, "modulate:a", 0.4, hover_duration * 3)
 
 func smooth_hide() -> void:
-	area_3d.mouse_entered.disconnect(_on_mouse_entered)
-	area_3d.mouse_exited.disconnect(_on_mouse_exited)
-	clicked_on_area3d.clicked.disconnect(_on_clicked)
+	clickable_area_3d.mouse_entered.disconnect(_on_mouse_entered)
+	clickable_area_3d.mouse_exited.disconnect(_on_mouse_exited)
+	clickable_area_3d.clicked.disconnect(_on_clicked)
 	
 	_stop_tween_if_needed()
 	tween = create_tween()
