78f65febcaf3742448bbf17181a9c59b5b814bb0
scrawach <scrawach@gmail.com>
2025-09-11 13:21:09 +0300

    feat: make cursor autoload and change cursor on clickable area3d

--- src/common/interactions/clickable_area3d.gd
+++ src/common/interactions/clickable_area3d.gd
@@ -5,6 +5,8 @@ signal clicked()
 
 func _ready() -> void:
 	input_event.connect(_on_input_event)
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event.is_action_pressed("interact"):
@@ -15,3 +17,9 @@ func enable() -> void:
 
 func disable() -> void:
 	input_ray_pickable = false
+
+func _on_mouse_entered() -> void:
+	Cursor.make_interaction()
+
+func _on_mouse_exited() -> void:
+	Cursor.clear()
