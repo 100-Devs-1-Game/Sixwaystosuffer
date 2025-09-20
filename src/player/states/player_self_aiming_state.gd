0f8df666289696c8637c2c57fd668b42e37f62f6
scrawach <scrawach@gmail.com>
2025-09-20 13:59:01 +0300

    feat: add woosh audio effect for self and target aiming for player

--- src/player/states/player_self_aiming_state.gd
+++ src/player/states/player_self_aiming_state.gd
@@ -3,10 +3,12 @@ extends PlayerAimingState
 
 @export var player_hand_animation: AnimationPlayer
 @export var tremor_animation: AnimationPlayer
+@export var woosh_audo_player: AudioStreamPlayer
 
 func enter_async() -> void:
 	player_hand_animation.play("self_aim")
 	tremor_animation.play("tremor")
+	woosh_audo_player.play()
 	await current_animation_ended(player_hand_animation)
 	is_ready_to_fire = true
 
@@ -15,4 +17,5 @@ func exit_async() -> void:
 	# TODO: make unique animation?
 	player_hand_animation.play_backwards("self_aim")
 	tremor_animation.play("idle")
+	woosh_audo_player.play()
 	await current_animation_ended(player_hand_animation)
